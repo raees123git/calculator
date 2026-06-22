c06866d2ec9f637c570097cafae6aa8ac8898e2e
Raees Zakir <zakirraees91@gmail.com>
2026-06-22 16:12:22 +0500

    Add variables a, j, and k to main.py

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ print("* : Multiplication")
 print("/ : Division")
 operation = input("Enter the operator (+, -, *, /): ")
 
+a="raees"
+j=5
+k=54
+
 # 3. Perform the calculation and show the output
 print("\n--- Result ---")
 if operation == "+":
