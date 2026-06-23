a749fe12ebf9f79ed9a58b14608046887d090a81
Raees Zakir <zakirraees91@gmail.com>
2026-06-23 11:26:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ print("* : Multiplication")
 print("/ : Division")
 operation = input("Enter the operator (+, -, *, /): ")
 
+a="raees"
+v=44
+print("helllo world")
+v
+
 # 3. Perform the calculation and show the output
 print("\n--- Result ---")
 if operation == "+":
