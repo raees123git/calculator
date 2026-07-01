2007d4eb413749dcd954c4cec6d9b28eb8489c9a
Raees Zakir <zakirraees91@gmail.com>
2026-07-01 18:31:27 +0500

    Add variables and modify main.py
    Added variables and improved code structure.

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ print("* : Multiplication")
 print("/ : Division")
 operation = input("Enter the operator (+, -, *, /): ")
 
+a="raees"
+d=4
+ads=3243
+asdfs
+
 # 3. Perform the calculation and show the output
 print("\n--- Result ---")
 if operation == "+":
