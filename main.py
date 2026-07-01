b94f798764ae52cd8d4d175ec0bf088136f2d3d8
Raees Zakir <zakirraees91@gmail.com>
2026-07-01 18:37:15 +0500

    Clean up main.py by removing unused variables
    Removed unused variables and corrected indentation.

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ print("* : Multiplication")
 print("/ : Division")
 operation = input("Enter the operator (+, -, *, /): ")
 
+a="raees"
+dsdfa=3435
+adsfdasfd
+asfd="aersadf"
+
 # 3. Perform the calculation and show the output
 print("\n--- Result ---")
 if operation == "+":
