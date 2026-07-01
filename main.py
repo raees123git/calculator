5e0f8690109cc1c96937c944ee46a3c07f0b0f14
Raees Zakir <zakirraees91@gmail.com>
2026-07-01 15:37:32 +0500

    Clean up main.py and fix variable assignments
    Removed invalid lines and fixed indentation.

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ print("* : Multiplication")
 print("/ : Division")
 operation = input("Enter the operator (+, -, *, /): ")
 
+a="raaees"
+asdf
+a=4
+
 # 3. Perform the calculation and show the output
 print("\n--- Result ---")
 if operation == "+":
