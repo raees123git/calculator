122128c07ca9696aa4b51376a0b8f90f43583a52
Raees Zakir <zakirraees91@gmail.com>
2026-07-02 17:11:34 +0500

    Fix formatting issues in main.py

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ print("* : Multiplication")
 print("/ : Division")
 operation = input("Enter the operator (+, -, *, /): ")
 
+a="raeess"
+aasdf=32423
+as=343
+
 # 3. Perform the calculation and show the output
 print("\n--- Result ---")
 if operation == "+":
