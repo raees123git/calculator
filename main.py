9dcbc22ab39cf355dde1954e874407a09196fd28
Raees Zakir <zakirraees91@gmail.com>
2026-07-02 11:00:01 +0500

    Clean up main.py by removing unused variables
    Removed unused variables and improved formatting.

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ print("* : Multiplication")
 print("/ : Division")
 operation = input("Enter the operator (+, -, *, /): ")
 
+a="raees"
+sdfa=3433
+asdf="Afdas"
+asdf=345
+
 # 3. Perform the calculation and show the output
 print("\n--- Result ---")
 if operation == "+":
