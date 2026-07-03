e00546fe2f157b22992420433ca06185b87f5867
Raees Zakir <zakirraees91@gmail.com>
2026-07-03 23:28:53 +0500

    Add new variables for calculation

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ print("* : Multiplication")
 print("/ : Division")
 operation = input("Enter the operator (+, -, *, /): ")
 
+raees="raees"
+wwe=619
+dsf=443
+
 # 3. Perform the calculation and show the output
 print("\n--- Result ---")
 if operation == "+":
