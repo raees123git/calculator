baf7cd55544ff8bb0ee2a5614de4a6f05b61f517
Raees Zakir <zakirraees91@gmail.com>
2026-07-03 16:07:21 +0500

    Add number divisibility check in testttt.py

---
+++ testttt.py
@@ -0,0 +1,13 @@
+# Take input from the user and convert it to an integer
+num = int(input("Enter a number: "))
+
+raees="zakir"
+ere=345656
+afdafs
+dsf=432
+
+# Check if the number is divisible by 2
+if num % 2 == 0:
+    print(f"{num} is an Even number.")
+else:
+    print(f"{num} is an Odd number.")
