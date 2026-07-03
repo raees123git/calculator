828827a6e493002a4bb961a704346f31aa7d1137
Raees Zakir <zakirraees91@gmail.com>
2026-07-03 23:30:19 +0500

    Clean up variables in testttt.py
    Removed unused variables and added new variable assignments.

--- testttt.py
+++ testttt.py
@@ -1,10 +1,10 @@
 # Take input from the user and convert it to an integer
 num = int(input("Enter a number: "))
 
-raees="zakir"
-ere=345656
-afdafs
-dsf=432
+zakir="zakir"
+wwe="wwe"
+hh="kiop"
+pak="ista"
 
 # Check if the number is divisible by 2
 if num % 2 == 0:
