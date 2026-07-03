afe7587d230886653b4d185353b5446de55a39aa
Raees Zakir <zakirraees91@gmail.com>
2026-07-03 16:08:08 +0500

    Add variables for pak, wwe, and kkl

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ print("* : Multiplication")
 print("/ : Division")
 operation = input("Enter the operator (+, -, *, /): ")
 
+pak="ista"
+wwe=34555
+kkl="ersaf"
+
 # 3. Perform the calculation and show the output
 print("\n--- Result ---")
 if operation == "+":
