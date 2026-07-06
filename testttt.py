1479c6280bdb7273ba4c5094178bdfda235ad66f
Raees Zakir <zakirraees91@gmail.com>
2026-07-06 13:27:04 +0500

    Clean up testttt.py by removing unused variables
    Removed unused variables and cleaned up code.

--- testttt.py
+++ testttt.py
@@ -1,11 +1,8 @@
 # Take input from the user and convert it to an integer
 num = int(input("Enter a number: "))
-
-raees="zakir"
-ere=345656
-afdafs
-dsf=432
-
+wwe="wwe"
+op="op"
+34rfds
 # Check if the number is divisible by 2
 if num % 2 == 0:
     print(f"{num} is an Even number.")
